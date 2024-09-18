bcf3fa20ac65c150bcff61174959d08055c7dda2
Mustafa Alotbah <mustafa.alotbah@gmail.com>
2024-09-18 19:09:32 +0200

    - Logger only slow for ERROR & WARN levels

--- PalmyraOS/include/core/peripherals/Logger.h
+++ PalmyraOS/include/core/peripherals/Logger.h
@@ -7,18 +7,25 @@
 
 #define DEBUG
 #define DEBUG_ALL
+//#define DEBUG_TRACE
 #define FUNCTION_DEC __FUNCTION__  // or __PRETTY_FUNCTION__
 
 #ifdef DEBUG
 
-#define LOG_INFO(...) PalmyraOS::kernel::log("INFO ", FUNCTION_DEC, __LINE__, __VA_ARGS__)
-#define LOG_WARN(...) PalmyraOS::kernel::log("WARN ", FUNCTION_DEC, __LINE__, __VA_ARGS__)
-#define LOG_ERROR(...) PalmyraOS::kernel::log("ERROR", FUNCTION_DEC, __LINE__, __VA_ARGS__)
+#define LOG_ERROR(...) PalmyraOS::kernel::log("ERROR", true, FUNCTION_DEC, __LINE__, __VA_ARGS__)
+#define LOG_WARN(...) PalmyraOS::kernel::log("WARN ", true, FUNCTION_DEC, __LINE__, __VA_ARGS__)
+#define LOG_INFO(...) PalmyraOS::kernel::log("INFO ", false, FUNCTION_DEC, __LINE__, __VA_ARGS__)
 
 #ifdef DEBUG_ALL
-#define LOG_DEBUG(...)  PalmyraOS::kernel::log("DEBUG", FUNCTION_DEC, __LINE__, __VA_ARGS__)
+#define LOG_DEBUG(...)  PalmyraOS::kernel::log("DEBUG", false, FUNCTION_DEC, __LINE__, __VA_ARGS__)
 #else
 #define LOG_DEBUG(...)
+#endif
+
+#ifdef DEBUG_TRACE
+#define LOG_TRACE(...)  PalmyraOS::kernel::log("TRACE", false, FUNCTION_DEC, __LINE__, __VA_ARGS__)
+#else
+#define LOG_TRACE(...)
 #endif
 
 #else
@@ -32,6 +39,6 @@ namespace PalmyraOS::kernel
 {
 
   /* Do not use directly, rather use the macros */
-  [[maybe_unused]] void log(const char* level, const char* function, uint32_t line, const char* format, ...);
+  [[maybe_unused]] void log(const char* level, bool slow, const char* function, uint32_t line, const char* format, ...);
 
 }
--- PalmyraOS/source/core/peripherals/Logger.cpp
+++ PalmyraOS/source/core/peripherals/Logger.cpp
@@ -10,34 +10,48 @@
 #include <cstdarg>
 
 
-PalmyraOS::kernel::ports::SlowBytePort loggingPort(0x3F8);
+PalmyraOS::kernel::ports::BytePort loggingPort(0x3F8);
 
-void log_msg(const char* message)
+void log_msg(bool slow, const char* message)
 {
 	uint64_t maxBuffer = 4096;
-	for (int i         = 0; i < maxBuffer; ++i)
+	if (slow)
 	{
-		if (message[i] == '\0') break;
-		loggingPort.write(message[i]);
+		// Slow output (for important logs)
+		for (int i = 0; i < maxBuffer; ++i)
+		{
+			PalmyraOS::kernel::CPU::delay(512 * 1024);
+			if (message[i] == '\0') break;
+			loggingPort.write(message[i]);
+		}
+	}
+	else
+	{
+		// Fast output (for frequent logs)
+		for (int i = 0; i < maxBuffer; ++i)
+		{
+			if (message[i] == '\0') break;
+			loggingPort.write(message[i]);
+		}
 	}
 }
 
-void log_msgf(const char* format, ...)
+void log_msgf(bool slow, const char* format, ...)
 {
 	va_list args;
 	va_start(args, format);
 	char buffer[4096];  // Adjust size as necessary for your needs
 	vsnprintf(buffer, sizeof(buffer), format, args);
 	va_end(args);
-	log_msg(buffer);
+	log_msg(slow, buffer);
 }
 
-void PalmyraOS::kernel::log(const char* level, const char* function, uint32_t line, const char* format, ...)
+void PalmyraOS::kernel::log(const char* level, bool slow, const char* function, uint32_t line, const char* format, ...)
 {
 	va_list args;
 	va_start(args, format);
 	char buffer[4096];  // Adjust size as necessary for your needs
 	vsnprintf(buffer, sizeof(buffer), format, args);
 	va_end(args);
-	log_msgf("%s [%s:%d] %s\n", level, function, line, buffer);
+	log_msgf(slow, "%s [%s:%d] %s\n", level, function, line, buffer);
 }
